f9333dbdf71f6ef46e32e67cc1e482c56355f539
Catapa <catalin24pap@gmail.com>
2021-08-23 18:02:18 +0300

    basic app

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import classes from './Modal.module.css'
+import Wrapper from '../../../hoc/Wrapper/Wrapper'
+import Backdrop from '../Backdrop/Backdrop'
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] will update')
+    }
+
+    render () {
+        return (
+            <Wrapper>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}>
+                    {this.props.children}
+                </div>
+            </Wrapper>
+        )
+    }
+
+}
+export default Modal
